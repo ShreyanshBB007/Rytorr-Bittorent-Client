3758fcddc82b309489fe40da356cbc77e7a00ebd
ShreyanshBB007 <shreyanshbhagwat007@gmail.com>
2026-03-19 22:53:08 +0530

    added many methods to piece_manager.py

--- piece_manager.py
+++ piece_manager.py
@@ -26,7 +26,6 @@ class PieceManager:
             raise ValueError("Piece count mismatch")
 
         self.pieces = []
-
         for i in range(self.total_pieces):
             if i == self.total_pieces - 1:
                 piece_len = self.file_length - ((self.total_pieces - 1) * self.piece_length)
@@ -40,10 +39,58 @@ class PieceManager:
             raise IndexError("Invalid piece index")
         return self.piece_hashes[index]
 
+    def get_next_block_request(self):
+        for piece_index in self.in_progress_pieces:
+            piece = self.pieces[piece_index]
+            block = piece.get_next_block()
+
+            if block is not None:
+                begin, length = block
+                return (piece_index, begin, length)
+
+        if self.missing_pieces:
+            piece_index = self.missing_pieces.pop()
+            self.in_progress_pieces.add(piece_index)
+
+            piece = self.pieces[piece_index]
+            block = piece.get_next_block()
+
+            if block is not None:
+                begin, length = block
+                return (piece_index, begin, length)
+
+        return None
+
+def handle_piece_received(self, piece_index, begin, data):
+    if piece_index < 0 or piece_index >= self.total_pieces:
+        return
+
+    if piece_index in self.completed_pieces:
+        return
+
+    piece = self.pieces[piece_index]
+
+    print(f"Received block for piece {piece_index}, begin={begin}")
+
+    piece.add_block(begin, data)
+
+    if piece.is_complete():
+        expected_hash = self.get_piece_hash(piece_index)
+
+        if piece.verify(expected_hash):
+            self.in_progress_pieces.discard(piece_index)
+            self.completed_pieces.add(piece_index)
+            print(f"Piece {piece_index} completed and verified")
+        else:
+            print(f"Piece {piece_index} failed verification, retrying")
+            piece.reset()
+            self.in_progress_pieces.discard(piece_index)
+            self.missing_pieces.add(piece_index)
+
 
 class Piece:
     def __init__(self, index, piece_length, block_size=16384):
-        
+
         self.index = index
         self.piece_length = piece_length
         self.block_size = block_size
@@ -51,6 +98,7 @@ class Piece:
         self.num_blocks = math.ceil(piece_length / block_size)
 
         self.blocks_received = [False] * self.num_blocks
+        self.blocks_requested = [False] * self.num_blocks
         self.blocks_data = [None] * self.num_blocks
 
     def add_block(self, begin, data):
@@ -63,6 +111,15 @@ class Piece:
             self.blocks_data[block_index] = data
             self.blocks_received[block_index] = True
 
+    def get_next_block(self):
+        for i in range(self.num_blocks):
+            if not self.blocks_received[i] and not self.blocks_requested[i]:
+                self.blocks_requested[i] = True
+                begin = i * self.block_size
+                length = min(self.block_size, self.piece_length - begin)
+                return (begin, length)
+        return None
+
     def is_complete(self):
         return all(self.blocks_received)
 
@@ -72,3 +129,8 @@ class Piece:
     def verify(self, expected_hash):
         piece_data = self.assemble()
         return hashlib.sha1(piece_data).digest() == expected_hash
+
+    def reset(self):
+        self.blocks_received = [False] * self.num_blocks
+        self.blocks_requested = [False] * self.num_blocks
+        self.blocks_data = [None] * self.num_blocks
